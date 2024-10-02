4ac98f367d51594434941db87f02df6519eb7b56
Alejandro Rubio <rubio.alejandro7@gmail.com>
2024-10-02 14:07:40 -0500

    fix svg draw lines and mobile styling

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -515,7 +515,7 @@ export default function Grid() {
           )}
         </div>
 
-        <div className="w-1/2 flex flex-col items-center justify-between mb-4">
+        <div className="w-full flex flex-col items-center justify-between mb-4">
           <div className="w-full mb-4">
             <h3 className="text-lg font-semibold mb-2 text-center">
               Hint Progress
